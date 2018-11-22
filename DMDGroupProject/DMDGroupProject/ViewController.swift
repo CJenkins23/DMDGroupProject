868cc68b4e61dbdb7f3e42058dbc645f55a9bf6e
Charleigh-Ann Jenkins (s4908563) <s4908563@mac42945.student.bournemouth.ac.uk>
2018-11-22 12:30:46 +0000

    added Xcode

---
+++ DMDGroupProject/DMDGroupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DMDGroupProject
+//
+//  Created by Charleigh-Ann Jenkins (s4908563) on 22/11/2018.
+//  Copyright © 2018 Charleigh-Ann Jenkins (s4908563). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
